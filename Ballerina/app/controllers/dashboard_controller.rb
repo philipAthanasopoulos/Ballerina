a120d865834c48ab6e3fcb365b2ca4a99a5c4a6f
philipAthanasopoulos <filipposfivos@gmail.com>
2025-05-08 12:15:19 +0300

    optimized queries for leaderboards and gmaes per year load, added score/Area_Sqrm plots

--- Ballerina/app/controllers/dashboard_controller.rb
+++ Ballerina/app/controllers/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class DashboardController < ApplicationController
   def index
     @year = params[:year].present? ? params[:year].to_i : Time.current.year
-    @games = Game.where("EXTRACT(YEAR FROM date) = ?", @year)
+    @games = Game.select('games.*, home_countries."Display_Name" AS home_team_name, away_countries."Display_Name" AS away_team_name')
+                 .joins('INNER JOIN countries AS home_countries ON games.home_team_id = home_countries.id')
+                 .joins('INNER JOIN countries AS away_countries ON games.away_team_id = away_countries.id')
+                 .where('EXTRACT(YEAR FROM games.date) = ?', @year)
   end
 end
